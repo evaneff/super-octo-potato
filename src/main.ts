2295d4522ba261a20471f7e7e238f20745b01789
Eva Neff <evaneff@gmail.com>
2024-09-16 21:03:49 -0600

    monday

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true //security feature to block insertion of extra fields
+    })
+  );
 }
 bootstrap();
